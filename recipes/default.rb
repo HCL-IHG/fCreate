03609da544698d3b9745dba70c0b6d5f0b0dbb76
Parmy <hcl.ihg@gmail.com>
2022-12-21 18:02:54 +0000

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+#node['ipaddress']
+#node['memory']['total']
+
+#print statement I have 4 apples
+apple_count = 4
+puts "I have #{apple_count} apples"
+
+file '/root/fCreate.txt' do
+  content "/fCreate/root.txt'
+  Hostname: #{node['hostname']} 
+  IPADDRESS: #{node['ipaddress']} 
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}" 
+end
+#include_recipe 'fUpdate::uRecipe'
